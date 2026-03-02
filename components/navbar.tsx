8f75fbce6cbafd2f27585215651c96ad1cb169ec
Vamshi <vamshivamsharaj123@gmail.com>
2026-03-03 03:43:44 +0530

    feat(auth): add sign out functionality

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -69,6 +69,7 @@ export default function Navbar() {
                                             <p>{session.user.email}</p>
                                         </div>
                                     </DropdownMenuLabel>
+                                    <SignOutButton />
                                 </DropdownMenuContent>
                             </DropdownMenu>
                         </>) :
