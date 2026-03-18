033d73923b03aa90d1eaf311420ff41d7839d082
Vamshi <vamshivamsharaj123@gmail.com>
2026-03-18 22:58:59 +0530

    feat(dashboard): implement post task page for creating new tasks

--- app/dashboard/post-task/page.tsx
+++ app/dashboard/post-task/page.tsx
@@ -1,7 +1,12 @@
+import CreateTaskForm from "@/components/tasks/TaskForm";
 export default function SavedPage() {
     return (
-        <div>
-            <h1 className="text-2xl font-bold">Post a Task</h1>
+
+        <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 py-12 px-6 flex justify-center">
+            <div className="w-full max-w-5xl">
+                <CreateTaskForm />
+            </div>
         </div>
+
     )
 }
